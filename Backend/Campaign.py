c03b46b8b901ecd041154c9840a242a5c854dbf4
majdyousof <117210724+majdyousof@users.noreply.github.com>
2023-04-06 11:42:35 +0100

    user and challenge class

---
+++ Backend/Campaign.py
@@ -0,0 +1,35 @@
+import numpy
+import pandas
+import time
+
+class User():
+
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        self.numofpoints = 0
+
+    def setUsername(self,username):
+        self.username = username
+
+    def setPassword(self,password):
+        self.password = password
+
+    def addpoints(self, addpoints):
+       self.numofpoints = self.numofpoints+addpoints
+
+
+class Challenge():
+
+    def __init__(self,objective,duration,points):
+
+        self.users = []
+        self.objective = objective
+        self.duration = duration
+        self.energyval = points
+
+
+    def adduser(self,user):
+        self.users.append(user.username)
+
+    
