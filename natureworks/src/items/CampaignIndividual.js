5ab007dff0a5f0cf8c317a969f29e2b97e3ade7e
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2023-04-06 15:03:16 +0100

    first page

--- natureworks/src/items/CampaignIndividual.js
+++ natureworks/src/items/CampaignIndividual.js
@@ -1,10 +1,20 @@
-import React from 'react'
-
+import React from "react";
 
 import { Link } from "react-router-dom";
 
-export default function CampaignIndividual({content}) {
+export default function CampaignIndividual({ date, title, description, backgroundColor }) {
+
   return (
-    <div>CampaignIndividual</div>
-  )
+    <div className="campaignbox">
+      <Link to="/campaign">
+        <div className="box" style={{ "background-color": "#C4F5C3" }}>
+          <div className="boxMargin">
+            <div className="date">{date}</div>
+            <div className="title">{title}</div>
+            <div className="description">{description}</div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 }
--- natureworks/src/pages/Campaign.js
+++ natureworks/src/pages/Campaign.js
@@ -3,10 +3,16 @@ import React from "react";
 import CampaignIndividual from "../items/CampaignIndividual";
 
 export default function Campaign() {
+
   return (
-    <div>
-      Campaign
-      <CampaignIndividual />
+    <div className="campaignList"> 
+      <CampaignIndividual date={"23 Jun 2023"} title={"23 Jun 2023"} description={"23 Jun 2023"} backgroundColor={"#C4F5C3"}/>
+      <CampaignIndividual date={"23 Jun 2023"} title={"23 Jun 2023"} description={"23 Jun 2023"} backgroundColor={"#C4F5C3"}/>
+      <CampaignIndividual date={"23 Jun 2023"} title={"23 Jun 2023"} description={"23 Jun 2023"} backgroundColor={"#C4F5C3"}/>
+      <CampaignIndividual date={"23 Jun 2023"} title={"23 Jun 2023"} description={"23 Jun 2023"} backgroundColor={"#C4F5C3"}/>
+      <CampaignIndividual date={"23 Jun 2023"} title={"23 Jun 2023"} description={"23 Jun 2023"} backgroundColor={"#C4F5C3"}/>
+      <CampaignIndividual date={"23 Jun 2023"} title={"23 Jun 2023"} description={"23 Jun 2023"} backgroundColor={"#C4F5C3"}/>
+
     </div>
   );
 }
