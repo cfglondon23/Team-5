662ed49f447b08c93c260a53c8fff7d026fbd936
majdyousof <117210724+majdyousof@users.noreply.github.com>
2023-04-06 17:34:44 +0100

    dict changes

--- Backend/api.py
+++ Backend/api.py
@@ -5,15 +5,33 @@ from Campaign import User, Challenge
 app = Flask(__name__)
 CORS(app)
 
-citydata = [{'London': {'Numofpoints':'1000'}},
-            {'Beijing': {'Numofpoints':'980'}},
-            {'Glasgow': {'Numofpoints': '900'}},
-            {'Mumbai': {'Numofpoints': '800'}},
-            {'Singapore': {'Numofpoints': '700'}},
-            {'Tehran': {'Numofpoints': '600'}},
-            {'Dubai': {'Numofpoints': '500'}},
-            {'Buenos Aires': {'Numofpoints': '400'}}]
+citydata = [{'City':'London', 'Numofpoints':'1000','Rank':'1'},
+            {'City':'Beijing', 'Numofpoints':'980','Rank':'2'},
+            {'City':'Glasgow', 'Numofpoints': '900','Rank':'3'},
+            {'City':'Mumbai', 'Numofpoints': '800','Rank':'4'},
+            {'City':'Singapore', 'Numofpoints': '700','Rank':'5'},
+            {'City':'Tehran', 'Numofpoints': '600','Rank':'6'},
+            {'City':'Dubai', 'Numofpoints': '500','Rank':'7'},
+            {'City':'Shanghai', 'Numofpoints': '400','Rank':'8'}]
 
+challengedata = [{'Challenge1':{'Name':'Earth Hour',
+                                'Duration':'1',
+                                'Reward':'3'}},
+                {'Challenge2':{'Name':'Zero Waste',
+                                'Duration':'1',
+                                'Reward':'3'}},
+                {'Challenge3':{'Name':'Greenify Your Life',
+                                'Duration':'1',
+                                'Reward':'3'}},
+                {'Challenge4':{'Name':'Eco-Friendly',
+                                'Duration':'1',
+                                'Reward':'3'}},
+                {'Challenge5':{'Name':'Carbon Footprint',
+                                'Duration':'1',
+                                'Reward':'3'}},
+                {'Challenge6':{'Name':'Reduce, Reuse, Recycle',
+                                'Duration':'1',
+                                'Reward':'3'}}]
 
 @app.route('/userdata', methods=['GET'])
 def get_energydata():
